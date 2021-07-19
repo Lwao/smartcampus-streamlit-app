b54951edca20091de192f98bcc865ac0a0a25d6e
leavitt <levygsg@hotmail.com>
2021-07-19 15:48:19 -0300

    Removed statistics and whole calibration routine in dependant on if there is a file loaded

--- pages/calibration.py
+++ pages/calibration.py
@@ -144,82 +144,83 @@ def app():
         if uploaded_file is not None: 
             df = load_calibration_dataset(uploaded_file)
             st.write(df.head())
-        else: pass
-
-    with configuration:
-        st.header('Configurações da calibração')
-        st.markdown('Algumas configurações devem ser realizadas para que a calibração atenda os resultados esperados.')
-
-        degree_columns = st.beta_columns(6)
-
-        list_ = ['VA): ', 'VB): ', 'VC): ', 'IA): ', 'IB): ', 'IC): ']
-        for i in range(6): degList[i][0] = degree_columns[i].number_input(label='Grau do polinômio (' + list_[i], min_value=1)
-
-        df, coeff_dict, r_dict = full_regression(df, degList, condList, labels)
-
-    with results:
-        st.header('Resultados da calibração')
-        st.markdown('Aqui são apresentados os resultados numéricos e gráficos da calibração.')
-
-        st.write('**Coeficientes de correlação**: ')
-        for i in range(6): st.write(labelsReduced[i] + ': ' + str(r_dict[list(r_dict.keys())[i]]))
-        st.write('**Coeficientes das curvas de calibração**: ')
-        for i in range(6): st.write(labelsReduced[i] + ': ' + str(coeff_dict[list(coeff_dict.keys())[i]]))
-
-        st.write('**Resultados gráficos**: ')
-
-        # sorting dataset
-
-        dfPlot = df.copy()
-        graph1 = st.beta_container()
-        graph2 = st.beta_container()
-
-        for i in range(6):
-            dfPlot[labels[2*i]] = dfPlot[labels[2*i]].sort_values(ascending=True)
-            dfPlot[predLabels[i]] = dfPlot[predLabels[i]].sort_values(ascending=True)
-
-        fig1 = make_subplots(rows=2, cols=3, shared_xaxes=False, 
-                    subplot_titles=('Tensão A (V)','Tensão B (V)', 'Tensão C (V)', 
-                                      'Corrente A (A)', 'Corrente B (A)', 'Corrente C (A)'))
-        fig2 = make_subplots(rows=2, cols=3, shared_xaxes=False, 
-                            subplot_titles=('Tensão A','Tensão B', 'Tensão C', 
-                                            'Corrente A', 'Corrente B', 'Corrente C'))
-
-        preNames = ['VA-', 'VB-', 'VC-', 'IA-', 'IB-', 'IC-']   
-        label1 = 'predito'
-        label2 = 'lido'
-        for i in range(6):
-            x = int(np.floor(i/3))+1
-            y = int(i - 3*np.floor(i/3))+1
-
-            fig1.add_trace(
-                go.Scatter(x=dfPlot[labels[2*i+1]], y=dfPlot[predLabels[i]], name=preNames[i]+label1),
-                row=x, col=y
-            )
-            fig1.add_trace(
-                go.Scatter(x=dfPlot[labels[2*i+1]], y=dfPlot[labels[2*i]], name=preNames[i]+label2),
-                row=x, col=y
-            )
-
-            fig2.add_trace(
-                go.Scatter(y=100*(dfPlot[predLabels[i]]-dfPlot[labels[2*i+1]])/dfPlot[labels[2*i+1]], name='erro' + preNames[i]+label1),
-                row=x, col=y
-            )
-            fig2.add_trace(
-                go.Scatter(y=100*(dfPlot[labels[2*i]]-dfPlot[labels[2*i+1]])/dfPlot[labels[2*i+1]], name='erro' + preNames[i]+label2),
-                row=x, col=y
-            )
-            
         
+        # everything else depends on if a file was loaded
+            with configuration:
+                st.header('Configurações da calibração')
+                st.markdown('Algumas configurações devem ser realizadas para que a calibração atenda os resultados esperados.')
 
-        fig1.update_layout(height=700, width=1100, title_text='Predição/Lido vs. Real')
-        fig1.update_xaxes(title_text='Real lido no multimedidor', showgrid=True)
-        fig2.update_layout(height=700, width=1100, title_text='Erro relativo da Predição/Lido vs. Real')
+                degree_columns = st.beta_columns(6)
 
-        fig1['layout']['yaxis']['title']='Predição/Lido'
-        fig1['layout']['yaxis4']['title']='Predição/Lido'
-        fig2['layout']['yaxis']['title']='Erro relativo (%)'
-        fig2['layout']['yaxis4']['title']='Erro relativo (%)'
+                list_ = ['VA): ', 'VB): ', 'VC): ', 'IA): ', 'IB): ', 'IC): ']
+                for i in range(6): degList[i][0] = degree_columns[i].number_input(label='Grau do polinômio (' + list_[i], min_value=1)
 
-        graph1.write(fig1)
-        graph2.write(fig2)
+                df, coeff_dict, r_dict = full_regression(df, degList, condList, labels)
+
+            with results:
+                st.header('Resultados da calibração')
+                st.markdown('Aqui são apresentados os resultados numéricos e gráficos da calibração.')
+
+                st.write('**Coeficientes de correlação**: ')
+                for i in range(6): st.write(labelsReduced[i] + ': ' + str(r_dict[list(r_dict.keys())[i]]))
+                st.write('**Coeficientes das curvas de calibração**: ')
+                for i in range(6): st.write(labelsReduced[i] + ': ' + str(coeff_dict[list(coeff_dict.keys())[i]]))
+
+                st.write('**Resultados gráficos**: ')
+
+                # sorting dataset
+
+                dfPlot = df.copy()
+                graph1 = st.beta_container()
+                graph2 = st.beta_container()
+
+                for i in range(6):
+                    dfPlot[labels[2*i]] = dfPlot[labels[2*i]].sort_values(ascending=True)
+                    dfPlot[predLabels[i]] = dfPlot[predLabels[i]].sort_values(ascending=True)
+
+                fig1 = make_subplots(rows=2, cols=3, shared_xaxes=False, 
+                            subplot_titles=('Tensão A (V)','Tensão B (V)', 'Tensão C (V)', 
+                                            'Corrente A (A)', 'Corrente B (A)', 'Corrente C (A)'))
+                fig2 = make_subplots(rows=2, cols=3, shared_xaxes=False, 
+                                    subplot_titles=('Tensão A','Tensão B', 'Tensão C', 
+                                                    'Corrente A', 'Corrente B', 'Corrente C'))
+
+                preNames = ['VA-', 'VB-', 'VC-', 'IA-', 'IB-', 'IC-']   
+                label1 = 'predito'
+                label2 = 'lido'
+                for i in range(6):
+                    x = int(np.floor(i/3))+1
+                    y = int(i - 3*np.floor(i/3))+1
+
+                    fig1.add_trace(
+                        go.Scatter(x=dfPlot[labels[2*i+1]], y=dfPlot[predLabels[i]], name=preNames[i]+label1),
+                        row=x, col=y
+                    )
+                    fig1.add_trace(
+                        go.Scatter(x=dfPlot[labels[2*i+1]], y=dfPlot[labels[2*i]], name=preNames[i]+label2),
+                        row=x, col=y
+                    )
+
+                    fig2.add_trace(
+                        go.Scatter(y=100*(dfPlot[predLabels[i]]-dfPlot[labels[2*i+1]])/dfPlot[labels[2*i+1]], name='erro' + preNames[i]+label1),
+                        row=x, col=y
+                    )
+                    fig2.add_trace(
+                        go.Scatter(y=100*(dfPlot[labels[2*i]]-dfPlot[labels[2*i+1]])/dfPlot[labels[2*i+1]], name='erro' + preNames[i]+label2),
+                        row=x, col=y
+                    )
+                    
+                
+
+                fig1.update_layout(height=700, width=1100, title_text='Predição/Lido vs. Real')
+                fig1.update_xaxes(title_text='Real lido no multimedidor', showgrid=True)
+                fig2.update_layout(height=700, width=1100, title_text='Erro relativo da Predição/Lido vs. Real')
+
+                fig1['layout']['yaxis']['title']='Predição/Lido'
+                fig1['layout']['yaxis4']['title']='Predição/Lido'
+                fig2['layout']['yaxis']['title']='Erro relativo (%)'
+                fig2['layout']['yaxis4']['title']='Erro relativo (%)'
+
+                graph1.write(fig1)
+                graph2.write(fig2)
+        else: pass
